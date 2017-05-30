1f456739b335ba3c342e9e76c6cdf2faf6c710ff
5743163246 <5743163246@pc190055m.ecc.u-tokyo.ac.jp>
2017-05-30 21:36:05 +0900

    implemented save discontinuous value and board

--- taiwan_cgi/main.cpp
+++ taiwan_cgi/main.cpp
@@ -8,12 +8,18 @@
 #include "tupleNetwork2.h"
 #include <iostream>
 #include "zhash.h"
+#include <cmath>
 using namespace std;
 
 double resultValue;
 ofstream writing_file;
+// ofstream discont_log;
 
 vector<board_t> TDLearning;
+
+// parameters for saving discontinuity
+double preValue = 0;
+Grid preGrid;
 
 board_t transformGridToBoard(Grid grid){
   board_t b = 0;
@@ -47,7 +53,7 @@ dir_e findBestMove(board_t b){
 	
   //clock_t startTime = clock();
   int startDepth = 2;
-  int endDepth = 2;
+  int endDepth = 4;
   if(iTupleSelect == 2) endDepth += 2;
   // iterative deepening
   while(startDepth <= endDepth){
@@ -139,6 +145,7 @@ void PlayNRounds(int n){
 	
   for(int i = 0;i < n;i++){
     writing_file.open("./gameValueOut/game"+to_string(i)+".txt", ios::out);
+    //discont_log.open("./discontinueLog/game"+to_string(i)+".txt", ios::out);
     //cout << "Playing Game: " << i;
     isGameOver = false;
     iTupleSelect = 0;
@@ -160,14 +167,27 @@ void PlayNRounds(int n){
 				
 	tempKey  = tempKey  >> 4;
       }
-			
       // checking if >= 8192 tile exist
       if(is8192Appear && is16384Appear) iTupleSelect = 2;
       else if(is8192Appear) iTupleSelect = 1;
 			
       dir = findBestMove(currentBoard);
+      
       // TDLearning here
       TDLearning.push_back(currentBoard);
+
+      // saving discontinuity
+      if (abs(preValue-resultValue)>10000 && preValue!=0) {
+	cout << "previous board:" << endl;
+	preGrid.print(35,2);
+	cout << "previous value: " << preValue << endl;
+	cout << "current board:" << endl;
+	currentGrid.print(35,2);
+	cout << "current value: " << resultValue << endl << endl;
+      }
+      preValue = resultValue;
+      preGrid = currentGrid;
+      
       myGame.insertDirection(dir);
       isGameOver = myGame.isGameOver();
 
@@ -176,18 +196,21 @@ void PlayNRounds(int n){
     myGame.printGrid(35,2);
     cout << endl;
     if(i < n - 1)  myGame.reset();
+    preValue = 0;
+    resultValue = 0;
     writing_file.close();
+    // discont_log.close();
   }
 }
 
 int main(int argc, char* argv[]){
   time_t seed = time(NULL);
-  time_t const_seed = 1495508584;
+  time_t const_seed = 1496138691;
   cout << "seed: " << seed << endl;
   cout << "const_seed: " << const_seed << endl;
   srand(const_seed);
   init();
-  PlayNRounds(5);
+  PlayNRounds(10);
 
   // cout << TDLearning[100] << endl;
 	
